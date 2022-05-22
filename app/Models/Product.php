5ed77f7bd9220b4d05c551f54627ea3290a14603
khanhpv195 <khanhpoly@gmail.com>
2022-05-22 21:16:38 +0700

    Update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,6 +15,6 @@ class Product extends Model
      * @var array
      */
     protected $fillable = [
-        'name', 'price','avatar','detail'
+        'name', 'price','avatar','detail','cate_id'
     ];
 }
